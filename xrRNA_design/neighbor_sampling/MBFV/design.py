22194644b5a136191c87fd2a4267cb5a0a959dfc
GutKat <katrin.gutenbrunner@gmail.com>
2024-10-22 08:56:06 +0200

    changed neighbourSampling

--- xrRNA_design/neighbor_sampling/MBFV/design.py
+++ xrRNA_design/neighbor_sampling/MBFV/design.py
@@ -3,10 +3,10 @@ import infrared.rna as rna
 import RNA
 import utils as ut
 import ir_utils as ir_ut
-import numpy as np
+import time
 
 # base Triples     -  +                   + -                                -     + 
-# db =        '..[[.((((((((((.......))))).((((((((.......{{{{{{{{.....))))))))]])))))......}}}}}}}}..'
+db =          '..[[.((((((((((.......))))).((((((((.......{{{{{{{{.....))))))))]])))))......}}}}}}}}..'
 iupac_cons =  'NNWGUCAGGCCXXXXNNNXXXXXXXXGCYACNXXXXXXXXXXXNNNXXXXXNXXXXXXXXNGUGCWGCCUGXXXXXXXXXXXNNNNN'
 structures = ['.....((((((((((.......))))).((((((((....................))))))))..)))))................',
 		      '..((............................................................)).....................',
@@ -14,54 +14,62 @@ structures = ['.....((((((((((.......))))).((((((((....................)))))))).
 #   		   0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 #    		   0        10        20        30        40        50        60        70        80
 
+# variable regions of the design
 var_stem_regions = [[(11, 14), (22, 25)], [(32, 35), (56, 59)], [(46, 50), (77, 81)]] # start, stop -> including stop
 var_loop_regions = [(18, 21), (36, 42), (52,55), (71, 76)] # start, stop -> including stop
+
+# alpha (0), beta (1), gamma (2) structure domains
 stems = {0: [(5, 10), (66, 71)], 1: [(10, 15), (22, 27)], 2:[(28, 36), (56, 64)]} # range
 loops = {'hl2': (15,22), 'hl3': (36, 56), 'upk1': (71, 77)} # range
-structure_span = {'stem': stems, 'loop': loops, 'ss':{'beta':(10, 27), 'gamma':(28, 64)}, 'gaps':{'beta':(12, 25), 'gamma':(32, 59)}}
+
+# all the different regions of the structure saved
+# ss and gaps are used for limiting the length of beta and gamma
+# ss is the whole range of the beta and gamma structure, gaps is the position of the first and last possible gap within the structure
+structure_span = {'stem': stems, 'loop': loops, 'ss':{'beta':(11, 27), 'gamma':(28, 64)}, 'gaps':{'beta':(11, 25), 'gamma':(32, 59)}}
 target_structure = structures[0]
 
-
-#monte carlo optimization of the sequence design - objective function is frequency of target structure
-def mc_optimization(model_input, target_structure, start=None, steps = 100000):
+# monte carlo optimization of the sequence design - objective function is frequency of target structure
+def mc_optimization(model_input, target_structure, start=None, steps=100000):
+    # create model
+    model = ir_ut.create_model(model_input)
     n = len(model_input.structures[0])
-    # create targeted model to find weights for energy and length feature
-    model_target = ir_ut.create_model_target(model_input)
-    
-    sampler = ir.Sampler(model_target)
-    sampler.set_target( 62, 10, 'totLength')
-    sampler.set_target( -18, 12, 'energy')
-    # sampler.set_target( -4, 3, 'energy_pk2')
-    sampler.targeted_sample() for _ in range(5000)
-    print("Weights",{k:f'{f.weight:.3f}' for k,f in sampler.model.features.items()})
-
-    # create model for sequence design
-    model = ir_ut.create_model(model_input, sampler.model.features)
 
     # start Monte carlo optimization
-    (best_ed, best_val), sampler = ut.mc_optimize(model,
-                                    model_input,
-                                    lambda sequence: ut.ensemble_defect(sequence, model_input),
-                                    round(steps * 0.5),
-                                    0.01,
-                                    start
-                                    )
+    tic = time.time()
+    sampler = ir.Sampler(model)
+    # sampler.set_target( 62, 10, 'totLength', -1)
+    sampler.set_target(62, 7, 'totLength', -1)
+    sampler.set_target( -18, 12, 'energy', -1)
+    (best, bestval),neighbors_top10, sampler = ut.neighbor_optimize_top10(sampler,
+                                                              model,
+                                                              model_input,
+                                                              lambda sequence: ut.ensemble_defect(sequence, model_input),
+                                                              round(steps),
+                                                              0.1,
+                                                              0.15,
+                                                              0.05,
+                                                              rna.HammingDist,
+                                                              start,
+                                                              )
 
+    (best, bestval),neighbors_top10, sampler = ut.neighbor_optimize_top10(sampler,
+                                                              model,
+                                                              model_input,
+                                                              lambda sequence: ut.target_frequency(sequence, model_input),
+                                                              round(steps),
+                                                              0.1,
+                                                              0.15,
+                                                              0.05,
+                                                              rna.HammingDist,
+                                                              neighbors_top10,
+                                                              )
 
-        # start Monte carlo optimization
-    (best, best_val), sampler = ut.mc_optimize(model,
-                                    model_input,
-                                    lambda sequence: ut.target_frequency(sequence, model_input),
-                                    round(steps * 0.5),
-                                    0.01,
-                                    best_ed
-                                    )
-
+    tac = time.time()
     # get sequence from the MC optimization
     sample = rna.values_to_seq(best.values()[:n])
     # get structure from sample
     culled_structures = [ut.remove_positioned_gaps(sample, s) for s in structures]
-    culled_seq = sample.replace('-','')
+    culled_seq = sample.replace('-', '')
 
     # get target-frequency, energy of PK2, ensemble frequency, MFE and predicted secondary structure
     fc = RNA.fold_compound(culled_seq)
@@ -71,13 +79,12 @@ def mc_optimization(model_input, target_structure, start=None, steps = 100000):
     pk2_e = fc.eval_structure(culled_structures[2])
     ed = fc.ensemble_defect(ss)
 
-    print("Weights",{k:f'{f.weight:.3f}' for k,f in sampler.model.features.items()})
     # print all information about best sample
     if True:
         print('\n')
         ut.margin_left('sequence:', sample, 30)
         ut.margin_left('IUPAC:', iupac_cons, 30)
-        ut.margin_left('target structure:', target_structure, 30)
+        ut.margin_left('target structure:', db, 30)
         print('\nculled')
         ut.margin_left('sequence:', culled_seq, 30)
         ut.margin_left('base structure:', culled_structures[0], 30)
@@ -87,126 +94,28 @@ def mc_optimization(model_input, target_structure, start=None, steps = 100000):
         print('\nRNAFold predictions')
         ut.margin_left('target structure:', culled_structures[0], 30)
         ut.margin_left('MFE structure:', ss, 30)
-        ut.margin_left('structure == MFE:', ss ==culled_structures[0], 30)
+        ut.margin_left('structure == MFE:', ss == culled_structures[0], 30)
         ut.margin_left('MFE:', f'{mfe:4.2f}', 30)
         ut.margin_left('length:', len(culled_seq), 30)
-        ut.margin_left('energy:', f"{RNA.energy_of_struct(culled_seq,culled_structures[0]):4.2f}", 30)
+        ut.margin_left('energy:', f"{RNA.energy_of_struct(culled_seq, culled_structures[0]):4.2f}", 30)
         ut.margin_left('PK2 energy:', f'{pk2_e:2.4f}', 30)
         ut.margin_left('frequency:', f'{freq:2.4f}', 30)
         ut.margin_left('ensemble defect:', f'{ed:2.4f}', 30)
 
 
-
+    print(f'time: {round((tac-tic)/60,3)}')
 
 def main():
     model_input = ir_ut.ModelInput(structures=structures,
-                                anti_structures=[],
-                                iupac=iupac_cons,
-                                var_stem_regions=var_stem_regions,
-                                var_loop_regions=var_loop_regions,
-                                structure_span = structure_span,
-                                target_length = target_len
-                                )
+                                   anti_structures=[],
+                                   iupac=iupac_cons,
+                                   var_stem_regions=var_stem_regions,
+                                   var_loop_regions=var_loop_regions,
+                                   structure_span=structure_span,
+                                   )
 
-    mc_optimization(model_input, target_structure=target_structure, steps=100000)
-
+    mc_optimization(model_input, target_structure=structures, steps=1)
 
 
 if __name__ == "__main__":
     main()
-
-
-####################################################
-# for testing the objective function and diversity #
-####################################################
-
-
-def testing(steps):
-    model_input = ir_ut.ModelInput(structures=structures,
-                                anti_structures=[],
-                                iupac=iupac_cons,
-                                var_stem_regions=var_stem_regions,
-                                var_loop_regions=var_loop_regions,
-                                structure_span = structure_span,
-                                target_length = target_len
-                                )
-    # create model
-    model = ir_ut.create_model(model_input)
-    n = len(model_input.structures[0])
-
-    # start Monte carlo optimization
-    (best_ed, best_val), sampler = result = ut.mc_optimize(model,
-                                    model_input,
-                                    lambda sequence: ut.ensemble_defect(sequence, model_input),
-                                    round(steps * 0.5),
-                                    0.01,
-                                    None
-                                    )
-
-
-        # start Monte carlo optimization
-    (best, best_val), sampler = result = ut.mc_optimize(model,
-                                    model_input,
-                                    lambda sequence: ut.target_frequency(sequence, model_input),
-                                    round(steps * 0.5),
-                                    0.01,
-                                    best_ed
-                                    )
-
-    return rna.values_to_seq(best.values()[:n])
-
-
-# #monte carlo optimization of the sequence design - objective function is frequency of target structure
-# # used for testing the objective function
-# def mc_optimization_testing(model_input, target_structure, start=None, steps = 100000, obj=1):
-#     model = ir_ut.create_model(model_input)
-#     n = len(model_input.structures[0])
-#     if obj==0:
-#         result = ut.mc_optimize(model,
-#                                         model_input,
-#                                         lambda sequence: ut.objective_function0(sequence, model_input),
-#                                         steps,
-#                                         0.01,
-#                                         start
-#                                         )
-#     elif obj==1:
-#         result = ut.mc_optimize(model,
-#                                         model_input,
-#                                         lambda sequence: ut.objective_function1(sequence, model_input),
-#                                         steps,
-#                                         0.01,
-#                                         start
-#                                         )
-#     elif obj==2:
-#         result = ut.mc_optimize(model,
-#                                         model_input,
-#                                         lambda sequence: ut.objective_function2(sequence, model_input),
-#                                         steps,
-#                                         0.01,
-#                  start
-#                                         )
-
-#     return rna.values_to_seq(best.values()[:n])
-
-
-
-# def testing(steps, objective_function):
-#     model_input = ir_ut.ModelInput(structures=structures,
-#                                 anti_structures=[],
-#                                 iupac=iupac_cons,
-#                                 var_stem_regions=var_stem_regions,
-#                                 var_loop_regions=var_loop_regions,
-#                                 structure_span = structure_span,
-#                                 target_length = target_len
-#                                 )
-
-#     sample = mc_optimization_testing(model_input, target_structure=target_structure, steps=steps, obj=objective_function)
-#     culled_structures = [ut.remove_positioned_gaps(sample, s) for s in structures]
-#     culled_seq = sample.replace('-','')
-#     fc = RNA.fold_compound(culled_seq)
-#     fc.pf()
-#     (ss, mfe) = fc.mfe()
-#     freq = fc.pr_structure(culled_structures[0])
-#     pk2_e = fc.eval_structure(culled_structures[2])
-#     ed = fc.ensemble_defect(ss)
-#     return sample, freq, ed, pk2_e
--- xrRNA_design/neighbor_sampling/MBFV/utils.py
+++ xrRNA_design/neighbor_sampling/MBFV/utils.py
@@ -7,11 +7,11 @@ import math
 import ir_utils as ir_ut
 import matplotlib.pyplot as plt
 import numpy as np
+import heapq
 
 
 def margin_left(left_text, right_text, padding):
     print(f"{left_text: <{padding}}{right_text}")
-
 
 
 def remove_positioned_gaps(sequence, structure):
@@ -22,72 +22,214 @@ def remove_positioned_gaps(sequence, structure):
 
 def add_gaps(sequence, structure):
     structure = list(structure)
-    new_ss = [structure.pop(0) if nt != '-' else '-' for  nt in sequence]
+    new_ss = [structure.pop(0) if nt != '-' else '-' for nt in sequence]
     return ''.join(new_ss)
 
 
 def target_frequency(sequence, model_input):
     ss = remove_positioned_gaps(sequence, model_input.structures[0])
-    fc = RNA.fold_compound(sequence.replace('-',''))
+    fc = RNA.fold_compound(sequence.replace('-', ''))
     fc.pf()
     return fc.pr_structure(ss)
 
 
 def ensemble_defect(sequence, model_input):
     ss = remove_positioned_gaps(sequence, model_input.structures[0])
-    fc = RNA.fold_compound(sequence.replace('-',''))
+    fc = RNA.fold_compound(sequence.replace('-', ''))
     fc.pf()
     return - fc.ensemble_defect(ss)
 
 
-def neighbor_optimize(sampler, objective, steps, temp, shift, variance, dist_func, start=None):
-    """Optimize by Monte-Carlo optimization with neighbor sampling
+def neighbor_optimize_top10(sampler, model, model_input, objective, steps, temp, shift, variance, dist_func, start=None):
+    n = len(model_input.structures[0])
+    curSampler = sampler
+    top_n = 10
+    steps = 50
 
-    Maximizes an objective function over assignments of a Infrared model
-    using a Monte-Carlo stochastic optimization strategy with
-    neighbor resampling and Metropolis criterion.
-    Targeted sampling is used.
+    if start == None:
+        curSample = curSampler.targeted_sample()
+        cur_seq = rna.values_to_seq(curSample.values()[:n])
+        curval = objective(cur_seq)
 
-    In each iteration, the algorithm samples for assignments which are in the
-    neighborhood of the currently selected assignment.
-    The distance function used to define the neighbohood as well as the extent of the
-    neighborhood must be provided. For RNA design, the hamming distance would be arecommendation
+        best, bestval = curSample, curval
+        oldVals = curSample.values()[:n]
+        curSampler = ir.getNeighborSampler(curSampler, oldVals, dist_func, shift, variance)
 
-    If no start assignment is given, a new one will be sampled from the given sampler.
+        top_neighbors = []
+        for _ in range(top_n-1):
+            new_neighbor = curSampler.targeted_sample()
+            top_neighbors.append(new_neighbor)
+        neighbors = [best, *top_neighbors] 
 
-    Args:
-        sampler: Infrared sampler describing assignments
-        objective: objective function on assignments
-        steps: iterations
-        temp: temperature for the Metropolis criterion
-        shift: The shift allowed for the neighborhood sampling. Given as a fraction of the number of variables.
-        variance: The variance allowed for the neighborhood sampling. Given as a fraction of the number of variables.
-        distanceFunction: A distance funtion which will be applied to each variable. Must take the variable of the assignemnt it compares to as an additional argument.
-        start: optional start assignment
+    else:
+        curSample = start[0]
+        oldVals = curSample.values()[:n]
+        top_neighbors = start
+        neighbors = [*top_neighbors] 
 
-    Returns:
-        Pair of best assignment and its objective value
-    """
+    old_top = top_neighbors
+           
 
-    currSampler = sampler
-    currSample = currSampler.targeted_sample() if start is None else start
-
-    cureval = objective(currSample)
-    best, besteval = currSample, cureval
-    newSample, neweval = currSample, cureval
-    oldVals = currSample.values()
-
-    currSampler = getNeighborSampler(currSampler, currSample.values(), dist_func, shift, variance)
-    currSampler.sample()
     for _ in range(steps):
-        if (currSample == newSample):
-            updateNeighborSampler(currSampler, currSample.values(), oldVals, dist_func, shift, variance)
-            oldVals = currSample.values()
-        newSample = currSampler.targeted_sample()
-        neweval = objective(newSample)
-        if (neweval >= cureval or random.random() <= math.exp((neweval-cureval)/temp)):
-            currSample, cureval = newSample, neweval
-            if cureval > besteval:
-                best, besteval = currSample, cureval
+        for neighbor in top_neighbors:
+            # print(rna.values_to_seq(neighbor.values()[:len(model_input.structures[0])]))
+            # neighborSampler = ir.Sampler(curSampler.model)
+            # neighborSampler = curSampler.copySampler()
 
-    return (best, besteval)
+            neighborVal = neighbor.values()[:n]
+            ir.updateNeighborSampler(curSampler, neighborVal, oldVals, dist_func, shift, variance)
+            
+            for _ in range(top_n):
+                # new_neighbor = curSampler.targeted_sample()
+                new_neighbor = curSampler.sample()
+                # print(rna.values_to_seq(new_neighbor.values()[:len(model_input.structures[0])]))
+                neighbors.append(new_neighbor)
+
+            oldVals = neighborVal
+
+
+        heap = []
+        for neighbor in neighbors:
+            neighbor_seq = rna.values_to_seq(neighbor.values()[:n])
+            neighbor_val = objective(neighbor_seq)
+            
+            if len(heap) < top_n:
+                heapq.heappush(heap, (neighbor_val, neighbor))
+            else:
+                heapq.heappushpop(heap, (neighbor_val, neighbor))
+
+        
+        top_elements = heapq.nlargest(top_n, heap)
+        top_neighbors = [item for _, item in top_elements]
+        best, best_seq, bestval = top_elements[0][1], rna.values_to_seq(top_elements[0][1].values()[:n]), top_elements[0][0]
+
+        print(redundant_elements(old_top, top_neighbors))
+        old_top = top_neighbors
+
+        bestVals = best.values()[:n]
+        ir.updateNeighborSampler(curSampler, bestVals, oldVals, dist_func, shift, variance)
+        oldVals = bestVals
+
+        print(best_seq, len(best_seq.replace('-','')), round(bestval,4))
+    
+    sampler = curSampler
+    return (best, bestval), top_neighbors, sampler
+
+
+# def neighbor_optimize_top10(sampler, model, model_input, objective, steps, temp, shift, variance, dist_func, start=None):
+#     n = len(model_input.structures[0])
+
+#     curSampler = sampler
+#     if start is None:
+#         top_neighbors = [curSampler.targeted_sample() for _ in range(10)] 
+#     else:
+#         top_neighbors = start
+
+#     bestval = -100
+#     best = None
+#     for neighbor in top_neighbors:
+#         neighbor_seq = rna.values_to_seq(neighbor.values()[:n])
+#         neighbor_val = objective(neighbor_seq)
+        
+#         if neighbor_val > bestval:
+#             bestval = neighbor_val
+#             best = neighbor
+
+#     # newSample, neweval = curSample, curval
+#     oldVals = best.values()[:n]
+#     curSampler = ir.getNeighborSampler(curSampler, oldVals, dist_func, shift, variance)
+
+#     old_top = top_neighbors
+#     neighbors = []
+
+#     for _ in range(2):
+#         for neighbor in top_neighbors:
+#             # print(rna.values_to_seq(neighbor.values()[:len(model_input.structures[0])]))
+#             # neighborSampler = ir.Sampler(curSampler.model)
+#             # neighborSampler = curSampler.copySampler()
+
+#             neighborVal = neighbor.values()[:n]
+#             ir.updateNeighborSampler(curSampler, neighborVal, oldVals, dist_func, shift, variance)
+            
+#             for _ in range(10):
+#                 # new_neighbor = curSampler.targeted_sample()
+#                 new_neighbor = curSampler.sample()
+#                 # print(rna.values_to_seq(new_neighbor.values()[:len(model_input.structures[0])]))
+#                 neighbors.append(new_neighbor)
+
+#             oldVals = neighborVal
+
+#         heap = []
+#         for neighbor in neighbors:
+#             neighbor_seq = rna.values_to_seq(neighbor.values()[:n])
+#             neighbor_val = objective(neighbor_seq)
+            
+#             if len(heap) < 10:
+#                 heapq.heappush(heap, (neighbor_val, neighbor))
+#             else:
+#                 heapq.heappushpop(heap, (neighbor_val, neighbor))
+
+        
+#         top_elements = heapq.nlargest(10, heap)
+#         top_neighbors = [item for _, item in top_elements]
+#         best, best_seq, bestval = top_elements[0][1], rna.values_to_seq(top_elements[0][1].values()[:n]), top_elements[0][0]
+
+
+#         print(redundant_elements(old_top, top_neighbors))
+#         old_top = top_neighbors
+
+#         # bestVals = best.values()[:n]
+#         # ir.updateNeighborSampler(curSampler, bestVals, oldVals, dist_func, shift, variance)
+#         # oldVals = bestVals
+
+#         print(best_seq, len(best_seq.replace('-','')), round(bestval,4))
+
+#     return (best, bestval),top_neighbors,  sampler
+        
+
+def redundant_elements(list1, list2):
+    # Convert both lists to sets to remove duplicates and then find intersection
+    set1 = set(list1)
+    set2 = set(list2)
+    
+    # Find the common elements
+    common_elements = set1.intersection(set2)
+    
+    # Return the number of common elements
+    return len(common_elements)
+
+
+
+def neighbor_optimize(sampler, model, model_input, objective, steps, temp, shift, variance, dist_func, start=None):
+    curSampler = sampler
+    curSample = curSampler.targeted_sample() if start is None else start
+
+    cur_seq = rna.values_to_seq(curSample.values()[:len(model_input.structures[0])])
+    curval = objective(cur_seq)
+
+    best, bestval = curSample, curval
+    newSample, neweval = curSample, curval
+    oldVals = curSample.values()
+
+    curSampler = ir.getNeighborSampler(curSampler, curSample.values(), dist_func, shift, variance)
+    curSampler.sample()
+    # for i in (range(steps)):
+    for i in tqdm(range(steps)):
+        if (curSample == newSample):
+            ir.updateNeighborSampler(curSampler, curSample.values(), oldVals, dist_func, shift, variance)
+            oldVals = curSample.values()
+        newSample = curSampler.targeted_sample()
+        new_seq = rna.values_to_seq(newSample.values()[:len(model_input.structures[0])])
+        newval = objective(new_seq)
+
+        if (newval >= curval or random.random() <= math.exp((newval - curval) / temp)):
+            # cur_seq = rna.values_to_seq(cur.values()[:len(model_input.structures[0])])
+            # print(new_seq == cur_seq)
+            curSample, curval = newSample, neweval
+            if curval > bestval:
+
+                best, bestval = curSample, curval
+    #            print(bestval)
+                best_seq = rna.values_to_seq(best.values()[:len(model_input.structures[0])])
+
+    return (best, bestval), sampler
