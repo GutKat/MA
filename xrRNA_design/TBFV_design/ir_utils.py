c046eacbfccdabc0c61a283fc0d1e6d5d41f7831
GutKat <katrin.gutenbrunner@gmail.com>
2024-10-28 17:23:57 +0100

    updating designs

--- xrRNA_design/TBFV_design/ir_utils.py
+++ xrRNA_design/TBFV_design/ir_utils.py
@@ -3,7 +3,7 @@ import infrared.rna as rna
 from collections import namedtuple
 import math
 
-ModelInput = namedtuple("ModelInput", "structures anti_structures iupac var_stem_regions var_loop_regions structure_span target_length")
+ModelInput = namedtuple("ModelInput", "structures anti_structures iupac var_stem_regions var_loop_regions gaps target_length")
 
 extended_iupac_nucleotides = {
     'A': 'A',
@@ -103,45 +103,32 @@ ir.def_constraint_class(
     lambda y0, y1, A, B: 1 if A<= (y1-y0) and (y1-y0) <= B else 0,
     "ir_utils"
 )
+ir.def_constraint_class(
+    'DiffInRangeAlpha',
+    lambda pos_i, pos_j, pos_k, pos_l, A, B, var, name:  var([(name, pos_i), (name, pos_j), (name, pos_k), (name, pos_l)]),
+    lambda y0, y1, y2, y3, A, B: 1 if A <= ((y1-y0) + (y3-y2)) <= B else 0, # A <=  ((y1-y0) + (y2-y3)) <= B
+    "ir_utils"
+)
 ########## END - Y for counting NT over possible gaps ###################
 
 
 ########## START - Y for calculating relations ###################
 ir.def_constraint_class(
-    'CalculateRelationStemHairpin',
-    lambda stem, stem_c, hl, hl_c, relation, var: [*var([('Y',stem[0]), ('Y',stem[1]), ('Y',stem[2]), ('Y',stem[3])]), *var([('Y',hl[0]), ('Y',hl[1])])],
-    lambda x0, x1, x2, x3, y0, y1, relation, stem_c, hl_c: 1 if ((y0 - y1) + hl_c) and number_in_relation(((x0-x1)+(x2-x3)+stem_c*2) / ((y0 - y1) + hl_c), relation) else 0,
+    # if two continous regions are calculated
+    'CalculateRelationI',
+    lambda pos_I, I_min, pos_II, II_min, relation, var, name:  var([(name, pos_I[0]), (name, pos_I[1]), (name, pos_II[0]), (name, pos_II[1])]),
+    lambda y0, y1, x0, x1, I_min, II_min, relation: 1 if ((x1-x0) + II_min) and number_in_relation( (((y1-y0) + I_min) / ((x1-x0) + II_min)), relation) else 0,
     "ir_utils"
 )
 ir.def_constraint_class(
-    'CalculateRelationStructure',
-    lambda stI, stI_c, stJ, stII_c, relation, var: [*var([('Y',stI[0]), ('Y',stI[1]), ('Y',stJ[0]), ('Y',stJ[1])])],
-    lambda x0, x1, y0, y1, relation, stI_c, stII_c: 1 if ((y0 - y1) + stII_c) and number_in_relation(((x0-x1)+stI_c) / ((y0 - y1) + stII_c), relation) else 0,
-    "ir_utils"
-)
-ir.def_constraint_class(
-    'CalculateRelationStructureI',
-    lambda stI, stI_c, stII, stII_c, relation, var: [*var([('Y',stI[0]), ('Y',stI[1]), ('Y',stI[2]), ('Y',stII[0]), ('Y',stII[1])])],
-    lambda x0, x1, x2, y0, y1, relation, stI_c, stII_c: 1 if ((y0 - y1) + stII_c) and number_in_relation(((x0-x1)+(x2)+stI_c*2) / ((y0 - y1) + stII_c), relation) else 0,
+    # if one region is not continous (first one is seperated)
+    'CalculateRelationII',
+    lambda pos_I, I_min, pos_II, II_min, relation, var, name:  var([(name, pos_I[0]), (name, pos_I[1]), (name, pos_I[2]), (name, pos_I[3]), (name, pos_II[0]), (name, pos_II[1])]),
+    lambda y0, y1, y2, y3, x0, x1, I_min, II_min, relation: 1 if ((x1-x0) + II_min) and number_in_relation( (((y1-y0) + (y3-y2) + I_min) / ((x1 - x0) + II_min)), relation) else 0,
     "ir_utils"
 )
 ########## END - Y for calculating relations ###################
 
-
-########## START - Z for counting As ###################
-ir.def_constraint_class(
-    'IncrementZIfA',
-    lambda x, pos, var: [*var([('X',x)]), *var([('Z',pos-1), ('Z',pos)])],
-    lambda nt, z0, z1: 1 if (nt == 0 and z0 + 1 == z1) or (nt != 0 and z0 == z1) else 0,
-    "ir_utils"
-)
-ir.def_constraint_class(
-    'ZSmallerThan',
-    lambda i, A, var: var([('Z',i-1)]),
-    lambda z, A: 1 if z <= A else 0,# p1 if x <= cutOff else 0 print(x, c)
-    "ir_utils"
-)
-########## End - Z for counting As ###################
 
 
 ########## Start - Energy class ###################
@@ -156,7 +143,7 @@ ir.def_function_class(
 
 def number_in_relation(x, relation):
     range_ = relations_range[relation]
-    if range_[0] <= round(x, 3) <= range_[1]:
+    if range_[0] <= x <= range_[1]:
         return 1
     return 0
 
@@ -168,21 +155,6 @@ def bpenergy(x, y, is_terminal=False):
     return (params_bp[bpidx//2 + (3 if is_terminal else 0)]
             if bpidx >= 0 else -math.inf)
 
-
-def hairpin_position_finding(loop, seq):
-    c = len(seq[loop[0]:loop[1]]) -seq[loop[0]:loop[1]].count('X')
-    p1 = seq[:loop[0]].count('X')
-    p2 = seq[:loop[1]].count('X')
-    return [p2, p1], c
-
-
-def stem_position_finding(stem, seq):
-    p = []
-    for s in stem:
-        c = len(seq[s[0]:s[1]]) -seq[s[0]:s[1]].count('X')
-        p.append(seq[:s[1]].count('X'))
-        p.append(seq[:s[0]].count('X'))
-    return p, c
 
 params_bp = [
     -0.52309,
@@ -219,18 +191,28 @@ bpindex_tab = [[-1, -1, -1, 0],
                [-1, 3, -1, 4],
                [1, -1, 5, -1]]
 
-#min max
+# min max
 relations_range = {
-        'st2_hl2': [0.6, 3.4],
-        'st3_hl3': [0.6, 3.5],
-        'sI_sII': [1.6, 2.9],
-        'sII_sIII': [0.5, 0.8],
-        'sI_sIII': [1.2, 1.5]
+        'stemG_hlG': [0.6, 1],
+        'A_B': [2.0, 2.75],
+        'A_G': [1.3, 1.4],
+        'B_G': [0.5, 0.7]
     }
 
 
 
-def create_model(model_input, feature_weights):
+def create_model(model_input):
+    ################## CREATE FEATURES ##################
+
+    model_target = create_features(model_input)
+    sampler = ir.Sampler(model_target)
+    # min = 84, max = 90, mean = 86
+    # need to add 2, have 2 nts in the beginning
+    # 86 - 92
+    sampler.set_target( 88, 4, 'totLength')
+    sampler.set_target( -30, 10, 'energy')
+    samples = [sampler.targeted_sample() for _ in range(1000)]
+    feature_weights = sampler.model.features
 
     ################## MODEL SET-UP ##################
 
@@ -291,64 +273,36 @@ def create_model(model_input, feature_weights):
         model.add_constraints(IncrementYIfNotGap(g, i, var))
 
 
-    
-    # ss_alpha = model_input.structure_span['gaps']['alpha']
-    ss_beta = model_input.structure_span['gaps']['beta']
-    ss_gamma = model_input.structure_span['gaps']['gamma']
+    gaps = model_input.gaps
 
-    # min_alpha = 2
-    # max_alpha = 8
-    min_beta = 1
-    max_beta = 6
-    min_gamma = 6
-    max_gamma = 8
+    min_max_alpha = [2, 8]
+    min_max_beta = [1, 6]
+    min_max_gamma = [6,8]
 
-    # # cur_min_alpha = 22
-    # # limit alpha to be between cur_min_alpha + 2 and cur_min_alpha + 8 --> 24 and 30
-    # pos_A = possible_gaps.index(ss_alpha[0]) + 1     # plus one because we have an artificial zero in variable Y
-    # pos_B = possible_gaps.index(ss_alpha[1]) + 1     # plus one because we have an artificial zero in variable Y
-    # model.add_constraints(DiffInRange(pos_A, pos_B, min_alpha, max_alpha, var, 'Y'))
+    alpha_pos = [possible_gaps.index(gaps['alpha'][i]) if i%2==0 else possible_gaps.index(gaps['alpha'][i])+1 for i in range(len(gaps['alpha'])) ]
+    beta_pos = [possible_gaps.index(gaps['beta'][i]) if i%2==0 else possible_gaps.index(gaps['beta'][i])+1 for i in range(len(gaps['beta'])) ]
+    gamma_pos = [possible_gaps.index(gaps['gamma'][i]) if i%2==0 else possible_gaps.index(gaps['gamma'][i])+1 for i in range(len(gaps['gamma'])) ]
+    stem_gamma_pos = [possible_gaps.index(gaps['gamma_stem'][i]) if i%2==0 else possible_gaps.index(gaps['gamma_stem'][i])+1 for i in range(len(gaps['gamma_stem'])) ]
+    hl_gamma_pos = [possible_gaps.index(gaps['gamma_hl'][i]) if i%2==0 else possible_gaps.index(gaps['gamma_hl'][i])+1 for i in range(len(gaps['gamma_hl'])) ]
+
+    # cur_min_alpha = 22
+    # limit alpha to be between cur_min_alpha + 2 and cur_min_alpha + 8 --> 24 and 30
+    model.add_constraints(DiffInRangeAlpha(*alpha_pos, *min_max_alpha, var, 'Y'))
 
     # cur_min_beta = 9
     # limit beta to be between cur_min_beta + 1 and cur_min_beta + 6 --> 10 and 15
-    pos_A = possible_gaps.index(ss_beta[0])      # plus one because we have an artificial zero in variable Y
-    pos_B = possible_gaps.index(ss_beta[1]) + 1     # plus one because we have an artificial zero in variable Y
-    model.add_constraints(DiffInRange(pos_A, pos_B, min_beta, max_beta, var, 'Y'))
 
-    # cur_min_gamma = 12
+    model.add_constraints(DiffInRange(*beta_pos, *min_max_beta, var, 'Y'))
+
+    # min_gamma = 12
     # limit gamma to be between cur_min_gamma + 6 and cur_min_gamma + 8 --> 18 and 20
-    pos_A = possible_gaps.index(ss_gamma[0])      # plus one because we have an artificial zero in variable Y
-    pos_B = possible_gaps.index(ss_gamma[1]) + 1    # plus one because we have an artificial zero in variable Y
-    model.add_constraints(DiffInRange(pos_A, pos_B, min_gamma, max_gamma, var, 'Y'))
+    model.add_constraints(DiffInRange(*gamma_pos, *min_max_gamma, var, 'Y'))
 
-    # ## constraints for relation
-    stems = model_input.structure_span['stem']
-    loops = model_input.structure_span['loop']
-    
-    stem_beta = stem_position_finding(stems[1], iupac)
-    stem_gamma = stem_position_finding(stems[2], iupac)
-
-    hl_beta = hairpin_position_finding(loops['hl2'], iupac)
-    hl_gamma = hairpin_position_finding(loops['hl3'], iupac)
-
-    model.add_constraints(CalculateRelationStemHairpin(stem_beta[0], stem_beta[1], hl_beta[0], hl_beta[1], 'st2_hl2', var))
-    model.add_constraints(CalculateRelationStemHairpin(stem_beta[0], stem_beta[1], hl_beta[0], hl_beta[1], 'st2_hl2', var))
-    model.add_constraints(CalculateRelationStemHairpin(stem_gamma[0], stem_gamma[1], hl_gamma[0], hl_gamma[1], 'st3_hl3', var))
-
-    sII = hairpin_position_finding([stems[1][0][0], stems[1][1][1]], iupac)
-    sIII = hairpin_position_finding([stems[2][0][0], stems[2][1][1]], iupac)
-
-    # stem I
-    a_,b_ = stem_position_finding(stems[0][0], iupac)
-    c_,d_  = stem_position_finding(stems[0][1], iupac)
-    sI_c = b_ + d_
-    sI = [c_[2], c_[1],  a_[2]], sI_c
-
-    model.add_constraints(CalculateRelationStructure(sII[0], sII[1], sIII[0], sIII[1], 'sII_sIII', var))
-    model.add_constraints(CalculateRelationStructureI(sI[0], sI[1], sII[0], sII[1], 'sI_sII', var))
-    model.add_constraints(CalculateRelationStructureI(sI[0], sI[1], sIII[0], sIII[1], 'sI_sIII', var))
-
-
+    # calculate relations between structure part
+    model.add_constraints(CalculateRelationII(alpha_pos, 22, beta_pos, 9, 'A_B', var, 'Y')) # alpha / beta
+    model.add_constraints(CalculateRelationII(alpha_pos, 22, gamma_pos, 12, 'A_G', var, 'Y')) # alpha / gamma
+    model.add_constraints(CalculateRelationI(beta_pos, 9, gamma_pos, 12, 'B_G', var, 'Y')) # beta / gamma
+    model.add_constraints(CalculateRelationII(stem_gamma_pos, 8, hl_gamma_pos, 4, 'stemG_hlG', var, 'Y')) # stem gamma / hl gamma
 
     if target_length:
         n_no_gaps = (len(possible_gaps) + target_length) - n
@@ -358,26 +312,9 @@ def create_model(model_input, feature_weights):
         model.add_functions([NotGap(i) for i in range(n)], 'totLength')
         model.set_feature_weight(feature_weights['totLength'].weight, 'totLength')
 
-
-    # ######## additional constraints from simulation analysis ########
-    # loops = model_input.structure_span['loop']
-    # upk1 = range(*loops['upk1'])
-    # n_z = len(upk1) + 1
-    # model.add_variables(n_z, n_z + 1, name='Z')
-
-    # model.restrict_domains([('Z',0)], (0, 0))
-    # for i in range(1, n_z):
-    #     model.restrict_domains([('Z',i)], (0, i-1))
-    # var = model.idx
-
-    # for i, pos in enumerate(upk1, start=1):
-    #     model.add_constraints(IncrementZIfA(pos, i, var))
-    # max_A = 8
-    # model.add_constraints(ZSmallerThan(n_z, max_A, var))
-
     return model
 
-def create_model_target(model_input):
+def create_features(model_input):
 
     ################## MODEL SET-UP ##################
     n = len(model_input.structures[0])
@@ -433,56 +370,37 @@ def create_model_target(model_input):
     for i, g in enumerate(possible_gaps, start=1):
         model.add_constraints(IncrementYIfNotGap(g, i, var))
 
-    # ss_alpha = model_input.structure_span['gaps']['alpha']
-    ss_beta = model_input.structure_span['gaps']['beta']
-    ss_gamma = model_input.structure_span['gaps']['gamma']
+    gaps = model_input.gaps
 
-    # min_alpha = 2
-    # max_alpha = 8
-    min_beta = 1
-    max_beta = 6
-    min_gamma = 6
-    max_gamma = 8
+    min_max_alpha = [2, 8]
+    min_max_beta = [1, 6]
+    min_max_gamma = [6,8]
+
+    alpha_pos = [possible_gaps.index(gaps['alpha'][i]) if i%2==0 else possible_gaps.index(gaps['alpha'][i])+1 for i in range(len(gaps['alpha'])) ]
+    beta_pos = [possible_gaps.index(gaps['beta'][i]) if i%2==0 else possible_gaps.index(gaps['beta'][i])+1 for i in range(len(gaps['beta'])) ]
+    gamma_pos = [possible_gaps.index(gaps['gamma'][i]) if i%2==0 else possible_gaps.index(gaps['gamma'][i])+1 for i in range(len(gaps['gamma'])) ]
+    stem_gamma_pos = [possible_gaps.index(gaps['gamma_stem'][i]) if i%2==0 else possible_gaps.index(gaps['gamma_stem'][i])+1 for i in range(len(gaps['gamma_stem'])) ]
+    hl_gamma_pos = [possible_gaps.index(gaps['gamma_hl'][i]) if i%2==0 else possible_gaps.index(gaps['gamma_hl'][i])+1 for i in range(len(gaps['gamma_hl'])) ]
+
+    # cur_min_alpha = 22
+    # limit alpha to be between cur_min_alpha + 2 and cur_min_alpha + 8 --> 24 and 30
+    model.add_constraints(DiffInRangeAlpha(*alpha_pos, *min_max_alpha, var, 'Y'))
 
     # cur_min_beta = 9
     # limit beta to be between cur_min_beta + 1 and cur_min_beta + 6 --> 10 and 15
-    pos_A = possible_gaps.index(ss_beta[0])     # plus one because we have an artificial zero in variable Y
-    pos_B = possible_gaps.index(ss_beta[1]) + 1     # plus one because we have an artificial zero in variable Y
-    model.add_constraints(DiffInRange(pos_A, pos_B, min_beta, max_beta, var, 'Y'))
+    model.add_constraints(DiffInRange(*beta_pos, *min_max_beta, var, 'Y'))
 
-    # cur_min_gamma = 12
+    # min_gamma = 12
     # limit gamma to be between cur_min_gamma + 6 and cur_min_gamma + 8 --> 18 and 20
-    pos_A = possible_gaps.index(ss_gamma[0])     # plus one because we have an artificial zero in variable Y
-    pos_B = possible_gaps.index(ss_gamma[1]) + 1     # plus one because we have an artificial zero in variable Y
-    model.add_constraints(DiffInRange(pos_A, pos_B, min_gamma, max_gamma, var, 'Y'))
+    model.add_constraints(DiffInRange(*gamma_pos, *min_max_gamma, var, 'Y'))
 
-    # ## constraints for relation
-    stems = model_input.structure_span['stem']
-    loops = model_input.structure_span['loop']
-    
-    stem_beta = stem_position_finding(stems[1], iupac)
-    stem_gamma = stem_position_finding(stems[2], iupac)
-
-    hl_beta = hairpin_position_finding(loops['hl2'], iupac)
-    hl_gamma = hairpin_position_finding(loops['hl3'], iupac)
+    # calculate relations between structure part
+    model.add_constraints(CalculateRelationII(alpha_pos, 22, beta_pos, 9, 'A_B', var, 'Y')) # alpha / beta
+    model.add_constraints(CalculateRelationII(alpha_pos, 22, gamma_pos, 12, 'A_G', var, 'Y')) # alpha / gamma
+    model.add_constraints(CalculateRelationI(beta_pos, 9, gamma_pos, 12, 'B_G', var, 'Y')) # beta / gamma
+    model.add_constraints(CalculateRelationII(stem_gamma_pos, 8, hl_gamma_pos, 4, 'stemG_hlG', var, 'Y')) # stem gamma / hl gamma
 
 
-    model.add_constraints(CalculateRelationStemHairpin(stem_beta[0], stem_beta[1], hl_beta[0], hl_beta[1], 'st2_hl2', var))
-    model.add_constraints(CalculateRelationStemHairpin(stem_beta[0], stem_beta[1], hl_beta[0], hl_beta[1], 'st2_hl2', var))
-    model.add_constraints(CalculateRelationStemHairpin(stem_gamma[0], stem_gamma[1], hl_gamma[0], hl_gamma[1], 'st3_hl3', var))
-
-    sII = hairpin_position_finding([stems[1][0][0], stems[1][1][1]], iupac)
-    sIII = hairpin_position_finding([stems[2][0][0], stems[2][1][1]], iupac)
-
-    # stem I
-    a_,b_ = stem_position_finding(stems[0][0], iupac)
-    c_,d_  = stem_position_finding(stems[0][1], iupac)
-    sI_c = b_ + d_
-    sI = [c_[2], c_[1],  a_[2]], sI_c
-
-    model.add_constraints(CalculateRelationStructure(sII[0], sII[1], sIII[0], sIII[1], 'sII_sIII', var))
-    model.add_constraints(CalculateRelationStructureI(sI[0], sI[1], sII[0], sII[1], 'sI_sII', var))
-    model.add_constraints(CalculateRelationStructureI(sI[0], sI[1], sIII[0], sIII[1], 'sI_sIII', var))
 
     if target_length:
         n_no_gaps = (len(possible_gaps) + target_length) - n
